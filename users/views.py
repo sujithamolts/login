1127e9522922721b9eac9e2d720207e7b2be5058
sujithamolts <sujithaanoop07@gmail.com>
2023-03-31 11:29:13 +0530

    First commit

---
+++ users/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages 
+from django contrib.auth.forms import UserCreationForm
+
+
+def home(request):
+    return render(request,'users/home.html')
+
+def register(request):
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form is_valid():
+            form.save()
+            messages.success(request,f"Your account has been created.You can login now!")
+            return redirect('login')
+    else:
+        form= UserCreationForm()
+    context {'form': form}
+    return render (request,'user/register.html',context)
+
+
+
+
+
+
